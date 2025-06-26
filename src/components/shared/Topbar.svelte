9e9e99cec9b424c31ea4ddd50ac4588bd7df4dd6
Mohammad Arif <programmermdarif@gmail.com>
2025-06-26 11:53:22 +0600

    done basic

---
+++ src/components/shared/Topbar.svelte
@@ -0,0 +1,7 @@
+<div class="relative bg-black px-4 py-2 text-center text-sm text-white">
+	<p>
+		Sign up and get 20% off to your first order.{' '}
+		<a href="/" class="font-medium underline"> Sign Up Now </a>
+	</p>
+	<button class="absolute top-2 right-4 text-white">×</button>
+</div>
---
+++ src/lib/data/Navbar.ts
@@ -0,0 +1,14 @@
+type TNavItem = {
+    label: string;
+    href: string;
+}
+
+
+
+export const NavData:TNavItem[] = [
+  { label: 'Shop', href: '/' },
+  { label: 'On Sale', href: '/' },
+  { label: 'New Arrivals', href: '/' },
+  { label: 'Brands', href: '/' },
+
+];
